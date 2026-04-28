76f98c88f8b61f3fc6629f02a8ebddf48eec2f27
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2026-04-28 17:23:25 +0200

    feat: implement resource removal and user registration features in Main class; enhance inventory display and search functionality

--- src/models/Main.java
+++ src/models/Main.java
@@ -1,4 +1,5 @@
 package models;
+
 import java.util.Scanner;
 
 public class Main {
@@ -21,7 +22,7 @@ public class Main {
             System.out.print("Scelta: ");
 
             scelta = scanner.nextInt();
-            scanner.nextLine(); 
+            scanner.nextLine();
 
             switch (scelta) {
 
@@ -64,8 +65,55 @@ public class Main {
                     }
                     break;
 
-                //TO DO case 2 general switch ecc...
-                }
-            }while(scelta!=6);
-        }
+                case 2:
+                    System.out.print("Inserisci codice della risorsa da rimuovere: ");
+                    String codRim = scanner.nextLine();
+
+                    Risorsa daRimuovere = null;
+                    for (Risorsa r : biblioteca.getListaRisorseDisponibili()) {
+                        if (r.getCodice().equalsIgnoreCase(codRim)) {
+                            daRimuovere = r;
+                            break;
+                        }
+                    }
+
+                    if (daRimuovere != null) {
+                        biblioteca.rimuoviRisorsa(daRimuovere);
+                    } else {
+                        System.out.println("Risorsa non trovata.");
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Nome utente: ");
+                    String nome = scanner.nextLine();
+
+                    System.out.print("ID utente: ");
+                    String id = scanner.nextLine();
+
+                    biblioteca.aggiungiUtente(new Utente(nome, id));
+                    break;
+
+                case 4:
+                    biblioteca.stampaInventario();
+                    break;
+
+                case 5:
+                    System.out.print("Titolo da cercare: ");
+                    String ricerca = scanner.nextLine();
+                    biblioteca.cercaPerTitolo(ricerca);
+                    break;
+
+                case 6:
+                    System.out.println("Uscita...");
+                    break;
+
+                default:
+                    System.out.println("Scelta non valida.");
+
+            }
+        } while (scelta != 6);
+
+        scanner.close();
+    }
 }
