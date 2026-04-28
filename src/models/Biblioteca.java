ead35020b9690dbe02f5aba5ae533a7406521172
TureCatedu <cataudellandrea@gmail.com>
2026-04-28 15:57:08 +0200

    class biblioteca

---
+++ src/models/Biblioteca.java
@@ -0,0 +1,38 @@
+package models;
+
+import java.util.Arraylist;
+
+public class Biblioteca {
+
+    private String nome;
+    private ArrayList<Risorse> listaRisorseDisponibili = new ArrayList<>();
+    private ArrayList<Utente> listaUtenti = new ArrayList<>();
+
+    public Biblioteca() {
+    }
+
+    public Biblioteca(String nome) {
+        this.nome = nome;
+    }
+
+    String getNome(){return this.nome;}
+    ArrayList<Risorse> getListaRisorseDisponibili(){return this.listaRisorseDisponibili;}
+    ArrayList<Utente> getListaUtenti(){return this.listaUtenti;}
+    
+    void setNome(String _nome){this.nome = _nome;}
+
+    public boolean aggiungiRisorsa(Risorsa risorsa)
+    {
+        for(Risorsa r : listaRisorseDisponibili)
+        {
+            if(r.getCodice().equalsIgnoreCase(risorsa))
+            {
+                System.out.println("Risorsa -> " + risorsa.getTitolo() + " già presente nella biblioteca - codice: [" + risorsa.getCodice() + "].");
+            }
+            else{
+                System.out.println("Risorsa -> " + risorsa.getTitolo() + " aggiunta alla biblioteca - codice: [" + risorsa.getCodice() + "]");
+
+            }
+        }
+    }
+}
